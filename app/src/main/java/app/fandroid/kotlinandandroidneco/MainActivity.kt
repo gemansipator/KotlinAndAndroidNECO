61efc05cc078ab3bbeeab21ac816a6cc09df7b9b
Nikolay PC house <gemansipator@gmail.com>
2021-03-29 20:06:07 +0400

    //   lateinit var tv : TextView  //аналог менее хорошего var tv : TextView? = null class MainActivity : AppCompatActivity() {     lateinit var tv : TextView  //аналог var tv : TextView? = null
        override fun onCreate(s: Bundle?) {
            super.onCreate(s)
            setContentView(R.layout.activity_main)
            tv = findViewById(R.id.vTest)
            tv.text = "ПРИВЕДДДДДД"
    
        }
    
        override fun onPause() {
            super.onPause()
            tv.text = "ПРИ ПАУЗЕ"
        }
    }

--- app/src/main/java/app/fandroid/kotlinandandroidneco/MainActivity.kt
+++ app/src/main/java/app/fandroid/kotlinandandroidneco/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         tv = findViewById(R.id.vTest)
         tv.text = "ПРИВЕДДДДДД"
-
     }
 
     override fun onPause() {
