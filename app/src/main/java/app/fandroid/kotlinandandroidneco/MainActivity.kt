c8a43ee2011857361290031988d47d6f4610c639
Nikolay PC house <gemansipator@gmail.com>
2021-03-29 18:46:43 +0400

    Слушатель кнопки без функции class MainActivity : AppCompatActivity() {     lateinit var bt : Button // вне функций, то в функции можно указать слушатель следующим образом:     override fun onCreate(s: Bundle?) {         super.onCreate(s)         setContentView(R.layout.activity_main)         var tv : TextView = findViewById(R.id.vTest)         tv.text = "ПРИВЕДДДДДД"         bt = findViewById(R.id.bTest)              //Сначала находим кнопку         bt.setOnClickListener {                    //СЛУШАТЕЛЬ   setOnClickListener             tv.text = "Hello!!!!!!!!!"         }     } }

--- app/src/main/java/app/fandroid/kotlinandandroidneco/MainActivity.kt
+++ app/src/main/java/app/fandroid/kotlinandandroidneco/MainActivity.kt
@@ -1,20 +1,22 @@
 package app.fandroid.kotlinandandroidneco
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import app.fandroid.kotlinandandroidneco.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var bidingClass : ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        bidingClass = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bidingClass.root)
-        bidingClass.vTest.text = "PPPPPPPPPp"
+    lateinit var bt : Button // вне функций, то в функции можно указать слушатель следующим образом:
+    override fun onCreate(s: Bundle?) {
+        super.onCreate(s)
+        setContentView(R.layout.activity_main)
+        var tv : TextView = findViewById(R.id.vTest)
+        tv.text = "ПРИВЕДДДДДД"
+        bt = findViewById(R.id.bTest)              //Сначала находим кнопку
+        bt.setOnClickListener {                    //СЛУШАТЕЛЬ   setOnClickListener
+            tv.text = "Hello!!!!!!!!!"
+        }
     }
-
-
 }
