66fd5d879d8b24f508e18f6615310331ffd59bff
Nikolay PC house <gemansipator@gmail.com>
2021-03-30 18:35:46 +0400

    //VISIBILITY
    class MainActivity : AppCompatActivity() {
        lateinit var bidingClass : ActivityMainBinding  //название может быть любое. Не только bindingClass. Binding превратил activity_main
        //в класс ActivityMain который заносится в переменную bindingClass
    
        override fun onCreate(s: Bundle?) {
            super.onCreate(s)
            bidingClass = ActivityMainBinding.inflate(layoutInflater) //Надувает класс/ делает активным и готовым для рисования , иннициализирует, заносит в память
            setContentView(bidingClass.root)                          //ПЕРЕДАЕТСЯ ИЗ bidingClass в класс MainActivity через ее функцию setContentView на
            //class MainActivity чтобы соединить с class MainActivity. Теперь это работает в class MainActivity. Пишем .root чтобы охватывало всЁ что есть
            // в ConstrainLayout
            bidingClass.bTest.setOnClickListener {
                bidingClass.vTest.visibility = View.GONE
            }
            bidingClass.bTest2.setOnClickListener {
                bidingClass.vTest.visibility = View.VISIBLE
            }
    
        }
    
        override fun onPause() {
            super.onPause()
            bidingClass.vTest.text = "НА ПАУЗЕ!!!!!!"
        }
    }

--- app/src/main/java/app/fandroid/kotlinandandroidneco/MainActivity.kt
+++ app/src/main/java/app/fandroid/kotlinandandroidneco/MainActivity.kt
@@ -2,6 +2,7 @@ package app.fandroid.kotlinandandroidneco
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -18,7 +19,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(bidingClass.root)                          //ПЕРЕДАЕТСЯ ИЗ bidingClass в класс MainActivity через ее функцию setContentView на
         //class MainActivity чтобы соединить с class MainActivity. Теперь это работает в class MainActivity. Пишем .root чтобы охватывало всЁ что есть
         // в ConstrainLayout
-        bidingClass.vTest.text = "Binding Works"
+        bidingClass.bTest.setOnClickListener {
+            bidingClass.vTest.visibility = View.GONE
+        }
+        bidingClass.bTest2.setOnClickListener {
+            bidingClass.vTest.visibility = View.VISIBLE
+        }
 
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        bidingClass.vTest.text = "НА ПАУЗЕ!!!!!!"
     }
 }
