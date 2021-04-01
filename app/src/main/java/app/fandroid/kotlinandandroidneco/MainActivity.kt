1e8827ba748ce107ca95edec7c26f2e367b801f6
gemansipator <gemansipator@gmail.com>
2021-04-02 00:56:39 +0400

    Log.d("MyLog", "Result = $resultValue")

--- app/src/main/java/app/fandroid/kotlinandandroidneco/MainActivity.kt
+++ app/src/main/java/app/fandroid/kotlinandandroidneco/MainActivity.kt
@@ -3,6 +3,7 @@ package app.fandroid.kotlinandandroidneco
 import android.graphics.Color
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -21,6 +22,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(bindingClass.root)                          //ПЕРЕДАЕТСЯ ИЗ bidingClass в класс MainActivity через ее функцию setContentView на
         //class MainActivity чтобы соединить с class MainActivity. Теперь это работает в class MainActivity. Пишем .root чтобы охватывало всЁ что есть
         // в ConstrainLayout
+
+        bindingClass.btResult.setOnClickListener {
+
+            val resultValue = bindingClass.edValue.text.toString().toInt()
+            Log.d("MyLog", "Result = $resultValue")
+
+
+        }
 
 
 
