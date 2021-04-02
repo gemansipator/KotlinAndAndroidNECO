a3796f94fc19d77ac241f84e4f40ac0a06834852
gemansipator <gemansipator@gmail.com>
2021-04-02 10:58:33 +0400

    Урок 11 минута 26
    val resultValue = bindingClass.edValue.text.toString().toInt()
                when(resultValue){
                    in 0..1000 -> {
                        bindingClass.tvResult.visibility = View.VISIBLE
                        bindingClass.tvResult.text = "Вы начинающий блогер"
                    }
    
                    in 1000..100000 -> {
                        bindingClass.tvResult.visibility = View.VISIBLE
                        bindingClass.tvResult.text = "Вы средний блогер"
                    }
    
                    else ->{
                        bindingClass.tvResult.visibility = View.VISIBLE
                        bindingClass.tvResult.text = "Вы супер-звезда"
                    }
                }
    
            }

--- app/src/main/java/app/fandroid/kotlinandandroidneco/MainActivity.kt
+++ app/src/main/java/app/fandroid/kotlinandandroidneco/MainActivity.kt
@@ -26,7 +26,22 @@ class MainActivity : AppCompatActivity() {
         bindingClass.btResult.setOnClickListener {
 
             val resultValue = bindingClass.edValue.text.toString().toInt()
-            Log.d("MyLog", "Result = $resultValue")
+            when(resultValue){
+                in 0..1000 -> {
+                    bindingClass.tvResult.visibility = View.VISIBLE
+                    bindingClass.tvResult.text = "Вы начинающий блогер"
+                }
+
+                in 1000..100000 -> {
+                    bindingClass.tvResult.visibility = View.VISIBLE
+                    bindingClass.tvResult.text = "Вы средний блогер"
+                }
+
+                else ->{
+                    bindingClass.tvResult.visibility = View.VISIBLE
+                    bindingClass.tvResult.text = "Вы супер-звезда"
+                }
+            }
 
 
         }
