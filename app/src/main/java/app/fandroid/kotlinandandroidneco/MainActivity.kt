a7cb133af1a86259d37fef234d495bdc93e0473b
Nikolay PC house <gemansipator@gmail.com>
2021-04-03 11:05:46 +0400

    Урок 11 и 12 ПРАВИЛЬНОЕ РЕШЕНИЕ домашнего задания
    class MainActivity : AppCompatActivity() {
        lateinit var bindingClass : ActivityMainBinding  //название может быть любое. Не только bindingClass. Binding превратил activity_main
        override fun onCreate(s: Bundle?) {
            super.onCreate(s)
            bindingClass = ActivityMainBinding.inflate(layoutInflater) //Надувает класс/ делает активным и готовым для рисования , иннициализирует, заносит в память
            setContentView(bindingClass.root)                          // ПЕРЕДАЕТСЯ ИЗ bidingClass в класс MainActivity через ее функцию setContentView на
            //class MainActivity чтобы соединить с class MainActivity. Теперь это работает в class MainActivity. Пишем .root чтобы охватывало всЁ что есть
            //  в ConstrainLayout
            bindingClass.btResult.setOnClickListener {
    
                val resultValue = bindingClass.edValue.text.toString()
                when(resultValue){
                    "Andrey" -> {
                        bindingClass.tvResult.visibility = View.VISIBLE
                        bindingClass.tvResult.text = "1000000"
                    }
                    "Sergey" -> {
                        bindingClass.tvResult.visibility = View.VISIBLE
                        bindingClass.tvResult.text = "3453453453435"
                    }
                    else ->{
                        bindingClass.tvResult.visibility = View.VISIBLE
                        bindingClass.tvResult.text = "Такого сотрудника нет"
                    }
                }
            }
        }
    }

--- app/src/main/java/app/fandroid/kotlinandandroidneco/MainActivity.kt
+++ app/src/main/java/app/fandroid/kotlinandandroidneco/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var bindingClass : ActivityMainBinding  //название может быть любое. Не только bindingClass. Binding превратил activity_main
     override fun onCreate(s: Bundle?) {
         super.onCreate(s)
-        bindingClass = ActivityMainBinding.inflate(layoutInflater) //Надувает класс/ делает активным и готовым для рисования , иннициализирует, заносит в память
+        bindingClass = ActivityMainBinding.inflate(layoutInflater) // Надувает класс/ делает активным и готовым для рисования , иннициализирует, заносит в память
         setContentView(bindingClass.root)                          // ПЕРЕДАЕТСЯ ИЗ bidingClass в класс MainActivity через ее функцию setContentView на
         //class MainActivity чтобы соединить с class MainActivity. Теперь это работает в class MainActivity. Пишем .root чтобы охватывало всЁ что есть
         //  в ConstrainLayout
@@ -20,11 +20,13 @@ class MainActivity : AppCompatActivity() {
             when(resultValue){
                 "Andrey" -> {
                     bindingClass.tvResult.visibility = View.VISIBLE
-                    bindingClass.tvResult.text = "Получите Ваши 1000000"
+                    val tempText = "Получите Ваши ${Constance.ANDREY_SUELDO}"
+                    bindingClass.tvResult.text = tempText
                 }
                 "Sergey" -> {
                     bindingClass.tvResult.visibility = View.VISIBLE
-                    bindingClass.tvResult.text = "Получите Ваши 3453453453435"
+                    val tempText2 = "Получите Ваши ${Constance.SEREY_SUELDO}"
+                    bindingClass.tvResult.text = tempText2
                 }
                 else ->{
                     bindingClass.tvResult.visibility = View.VISIBLE
@@ -32,5 +34,9 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+    object Constance{
+        const val ANDREY_SUELDO = 10000
+        const val SEREY_SUELDO = 10000
     }
 }
