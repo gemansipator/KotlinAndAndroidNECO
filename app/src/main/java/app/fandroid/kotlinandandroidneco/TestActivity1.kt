eb452d8410967cc1c3f2acd6960f1e8fe447dfd0
gemansipator <gemansipator@gmail.com>
2021-04-05 13:00:54 +0400

    Создание ACTIVITY || Урок 16    минута 25

---
+++ app/src/main/java/app/fandroid/kotlinandandroidneco/TestActivity1.kt
@@ -0,0 +1,28 @@
+package app.fandroid.kotlinandandroidneco
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class TestActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test1)
+    }
+
+    fun onClickGoMain(view: View){
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+    }
+    fun onClickGoTest2(view: View){
+
+        val intent = Intent(this, TestActivity2::class.java)
+        startActivity(intent)
+
+    }
+
+
+}
---
+++ app/src/main/java/app/fandroid/kotlinandandroidneco/TestActivity2.kt
@@ -0,0 +1,12 @@
+package app.fandroid.kotlinandandroidneco
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TestActivity2 : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_test_2)
+    }
+}
